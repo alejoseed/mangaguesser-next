48f77cf3fd41b9b1d23936f7aa9d06bc32bf7182
Alejandro <alejandrojpalmar@hotmail.com>
2023-06-07 11:38:29 -0400

    CSS fixes and code refractoring

--- src/pages/game/Play.tsx
+++ src/pages/game/Play.tsx
@@ -143,48 +143,46 @@ export default function Dev(){
   }
 
   return ( 
-    
+    <div className="flex flex-col h-full w-full lg:justify-center">
 
-    <div className="font-link">
-
-    <Head>
-      <title>MangaGuesser - Play</title>
-      <meta name="description" content="Showcase your manga skills!" />
-      <link rel="icon" href="/logo.svg" />
-      <link rel="preconnect" href="https://mangaguesser.up.railway.app" />
-
-      <meta property="og:title" content="MangaGuesser | Play Page" />
-      <meta property="og:description" content="Get a bunch of random manga and try to guess the correct one!" />
-      <meta property="og:image" content="https://mangaguesser.up.railway.app/mangaguesser-vercel-app-480x800phone.webp" />
-      <meta property="og:url" content="https://mangaguesser.vercel.app" />
-      <meta property="og:type" content="website" />
-    </Head>
-
-      <div className="mx-2">
-        <div className="flex flex-col space-x-4 items-center justify-center pt-4">
-          
-          {popUp && (
-            <>
-              <dialog open={popUp} 
-                      className="grid absolute z-10 opacity-100 bg-slate-500 w-96 h-48 justify-center items-center shadow-lg rounded-lg"
-                      >
-                <h1 className="text-white text-2xl">You are correct! +10 HP</h1>
-              </dialog>
-            </>
-          )}
-
-          <img src={mangaImageUrl} alt="NO MANGA FOUND" className="rounded-xl shadow-xl max-w-[330px] md:max-w-[360px]" width={500} height={500}/>
-          
-          <div className="mt-4 rounded-xl shadow-xl backdrop-blur bg-sky-300 font-link">
-            <p>Current streak:   {streak}</p>
-            <p>Current score:    {score} </p>
-            <p>Current HP:       {hp}    </p>
-          </div>
+        {popUp && (
+        <div className="font-link flex h-full w-full z-50 absolute opacity-80 bg-black justify-items-center">
+          <dialog open={true}
+            className="self-center text-center bg-cyan-400 rounded-lg w-[300px] h-[200px] flex justify-center items-center"
+          >
+            <div className="text-2xl ">
+              <p>You are correct! +10HP!</p>
+            </div>
+          </dialog>
         </div>
+        )}
 
-        <footer className="fixed left-0 bottom-0 w-full text-center">
-          <div className="grid gap-x-4 grid-cols-2 items-center pt-3">
-            <div className="grid grid-cols-1 gap-y-2">
+
+      <Head>
+        <title>MangaGuesser - Play</title>
+        <meta name="description" content="Showcase your manga skills!" />
+        <link rel="icon" href="/logo.svg" />
+        <link rel="preconnect" href="https://mangaguesser.up.railway.app" />
+
+        <meta property="og:title" content="MangaGuesser | Play Page" />
+        <meta property="og:description" content="Get a bunch of random manga and try to guess the correct one!" />
+        <meta property="og:image" content="https://mangaguesser.up.railway.app/mangaguesser-vercel-app-480x800phone.webp" />
+        <meta property="og:url" content="https://mangaguesser.vercel.app" />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      <div className="md:w-[660px] w-[360px] flex self-center mt-10">
+        <img className="w-full md:h-[660px] h-[360px] object-contain self-center text-center" src={mangaImageUrl} alt="No Manga Found" />
+      </div>
+
+      <div className="max-w-[130px] mt-4 rounded-xl shadow-xl backdrop-blur self-center bg-sky-300 font-link text-center justify-center">
+        <p>Current streak:   {streak}</p>
+        <p>Current score:    {score} </p>
+        <p>Current HP:       {hp}    </p>
+      </div>
+
+      <footer className="grid grid-cols-2 gap-x-2 p-3 w-full fixed bottom-0">
+          <div className="grid grid-cols-1 gap-y-2">
                 <button className="buttonManga w-full" onClick={() => handleAnswer(0)} style={{ backgroundColor: "#89CFF0", 
                 fontSize: manga.mangaOne.length > 41 ? '12px' : 'inherit' }}>
                 {manga.mangaOne}</button>
@@ -192,18 +190,22 @@ export default function Dev(){
                 fontSize: manga.mangaTwo.length > 41 ? '12px' : 'inherit'
                 }}>{manga.mangaTwo}</button>
             </div>
+
             <div className="grid grid-cols-1 gap-y-2">
+              
               <button className="buttonManga w-full" onClick={() => handleAnswer(2)} style={{ backgroundColor: "#36454F",
-              fontSize: manga.mangaThree.length > 41 ? '12px' : 'inherit'
-              }}>{manga.mangaThree}</button>
+                fontSize: manga.mangaThree.length > 41 ? '12px' : 'inherit'
+                }}>{manga.mangaThree}
+              </button>
+              
               <button className="buttonManga w-full" onClick={() => handleAnswer(3)} style={{ backgroundColor: "#009e60",
-              fontSize: manga.mangaFour.length > 41 ? '12px' : 'inherit'
-              }}>{manga.mangaFour}</button>
+                fontSize: manga.mangaFour.length > 41 ? '12px' : 'inherit'
+                }}>{manga.mangaFour}
+              </button>
             </div>
-          </div>
-        </footer>
-      
-      </div>
+
+      </footer>
+    
     </div>
   );
 }
