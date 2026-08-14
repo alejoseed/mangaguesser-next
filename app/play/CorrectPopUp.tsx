01ad8eaacc56cd30dd779cfe10707f10a2c0f086
alejoseed <alejandrojpalmar@hotmail.com>
2026-08-14 17:43:03 -0400

    missing hp from popup

--- app/play/CorrectPopUp.tsx
+++ app/play/CorrectPopUp.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function PopUp(){
     return (
         <dialog>
-            <h1 className="text-white text-2xl">You are correct! +10 HP</h1>
+            <h1 className="text-white text-2xl">You are correct!</h1>
         </dialog>
     )
 }
