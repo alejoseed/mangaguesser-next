81a34b1ce9300634dd6ba294966d8ec018cccf4b
alejoseed <alejandrojpalmar@hotmail.com>
2026-08-20 20:05:30 -0400

    added daily page for daily manga update

---
+++ app/daily/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import construction from "@/public/underconstruction.jpg"
+
+export default function Login(){
+    return(
+        <div className="grid grid-flow-col grid-cols-1 items-center" style={{height: "80svh"}}>
+            <div className="grid grid-flow-row justify-center">
+                <Image
+                    src={construction}
+                    alt="Error 404" 
+                    className="rounded-xl shadow" width={500} height={500}
+                />
+                
+                <h3 className="text-lg text-center">Sorry, you got here early. Will be done before 08/20/2026.</h3>
+                
+            </div>
+
+        </div>
+    );
+}
